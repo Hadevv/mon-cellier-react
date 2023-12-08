92132e1dc98b37597357e4cb17e46b4a12011fd9
Hadevv <91534880+Hadevv@users.noreply.github.com>
2023-12-08 21:02:31 +0100

    🎉 Initialisation du projet MonCellier

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from '@/pages/home';
+import { ThemeProvider } from '@/components/theme-provider';
+import Dashboard from '@/pages/dashboard';
+import Login from '@/pages/auth/login';
+import Register from '@/pages/auth/register';
+import NotFound from '@/404';
+import Projects from '@/pages/projects';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />,
+        errorElement: <NotFound />,
+    },
+    {
+        path: '/dashboard',
+        element: <Dashboard />,
+    },
+    {
+        path: '/login',
+        element: <Login />,
+    },
+    {
+        path: '/register',
+        element: <Register />,
+    },
+    {
+        path: '/projects',
+        element: <Projects />,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+            <RouterProvider router={router} />
+        </ThemeProvider>
+    </React.StrictMode>,
+);
