588e72b024f90b294ae9664fa77758119f29c85f
Hadevv <91534880+Hadevv@users.noreply.github.com>
2023-12-30 18:57:57 +0100

    🛠 package: shadcn/ui avatar auth

--- src/components/ui/avatar.jsx
+++ src/components/ui/avatar.jsx
@@ -10,7 +10,7 @@ const Avatar = React.forwardRef(({ className, ...props }, ref) => (
       "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
       className,
     )}
-    {...props}
+    {...props} // `as` already included in `Root`
   />
 ));
 Avatar.displayName = AvatarPrimitive.Root.displayName;
