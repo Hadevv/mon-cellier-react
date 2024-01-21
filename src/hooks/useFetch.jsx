18128bbef7d7a3e2cc000825ae038c7b94b78a21
Hadevv <91534880+Hadevv@users.noreply.github.com>
2024-01-21 21:26:38 +0100

    🧽clean : custom hook useFetch

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
